359cdda392809165c92b1eca6b7e65f6e013561d
Andromeda <geraskin19970309@gmail.com>
2023-11-24 11:37:58 +0200

    add workers to clean up, blur image and save to file

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,13 +19,23 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
+import com.example.bluromatic.workers.BlurWorker
+import com.example.bluromatic.workers.CleanupWorker
+import com.example.bluromatic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
+    private val workManager: WorkManager = WorkManager.getInstance(context)
+    private var imageUri: Uri = context.getImageUri()
+
 
     override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
@@ -33,7 +43,19 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * Create the WorkRequests to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
-    override fun applyBlur(blurLevel: Int) {}
+    override fun applyBlur(blurLevel: Int) {
+        val cleanup = OneTimeWorkRequest.from(CleanupWorker::class.java)
+        val blur = OneTimeWorkRequestBuilder<BlurWorker>()
+            .setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+            .build()
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>().build()
+        workManager.beginWith(cleanup)
+            .then(blur)
+            .then(save)
+            .enqueue()
+
+        // Start the work
+    }
 
     /**
      * Cancel any ongoing WorkRequests
